cbe106e67bcf4907e3fba7cd85b712ab48104641
jonniiik <zhek90@gmail.com>
2019-04-26 09:30:00 +0300

    Добавил отправление сообщений

--- app/src/main/java/com/eugeneponomarev/chatapp/MessageActivity.java
+++ app/src/main/java/com/eugeneponomarev/chatapp/MessageActivity.java
@@ -1,13 +1,126 @@
 package com.eugeneponomarev.chatapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.eugeneponomarev.chatapp.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MessageActivity extends AppCompatActivity {
+
+    CircleImageView profileImage;
+    TextView username;
+
+    ImageButton buttonSend;
+    EditText sendEditText;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        toolbarInitialization();
+        components();
+        firebaseInitialization();
+        startButton();
+
     }
+
+    private void startButton() {
+        final String userId = intent.getStringExtra("userId");
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = sendEditText.getText().toString();
+                if (!msg.equals("")) {
+                    sendMessage(firebaseUser.getUid(), userId, msg);
+                } else {
+                    Toast.makeText(MessageActivity.this, "You can't send empty message", Toast.LENGTH_SHORT).show();
+                }
+                sendEditText.setText(null);
+            }
+        });
+    }
+
+    private void toolbarInitialization() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    private void components() {
+        profileImage = (CircleImageView) findViewById(R.id.profileImage);
+        username = (TextView) findViewById(R.id.username);
+        buttonSend = (ImageButton) findViewById(R.id.buttonSend);
+        sendEditText = (EditText) findViewById(R.id.sendEditText);
+    }
+    private void firebaseInitialization(){
+        intent = getIntent();
+        final String userId = intent.getStringExtra("userId");
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(userId);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                assert user != null;
+                username.setText(user.getUsername());
+                if (user.getImageURL().equals("default")) {
+                    profileImage.setImageResource(R.drawable.mignon_batman);
+                } else {
+                    Glide.with(MessageActivity.this).load(user.getImageURL()).into(profileImage);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void sendMessage(String sender, String receiver, String message){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", sender);
+        hashMap.put("receiver", receiver);
+        hashMap.put("message", message);
+
+        reference.child("Chats").push().setValue(hashMap);
+    }
+
 }
